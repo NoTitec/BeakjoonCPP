afe83808bac089bcbd0d1138437a196b23e5aad9
NoTitec <zkxkr0@gmail.com>
2022-09-16 20:49:09 +0900

    init

---
+++ 1978_CPP/1978_CPP/1978_CPP.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int M, N, sum = 0;
+    int min_value=-1;
+    bool prime[10000];
+    fill_n(prime, 10000, 1);
+    prime[0] = false;
+    prime[1] = false;//1 is not prime number
+    cin >> M;
+    cin >> N;
+    //find prime number
+    for (int i = 2; i <= sqrt(N); i++)//Eratosthenes 
+        if (prime[i] == true)
+            for (int j = i * 2; j <= N; j += i)// delete i*N
+                prime[j] = false;
+
+    for (int i = M; i <= N; i++)
+        if (prime[i] == true)
+        {
+            if (min_value==-1)
+                min_value = i;
+            sum += i;
+        }
+    if (min_value==-1)
+        cout << "-1";
+    else
+        cout << sum << '\n' << min_value;
+    return 0;
+}
---
+++ 2581_CPP/2581_CPP/2581_CPP.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool prime[10000];
+int main() {
+    int M, N, sum = 0;
+    int min_value = -1;
+
+    fill_n(prime, 10000, 1);
+    prime[0] = false;
+    prime[1] = false;
+    cin >> M >> N;
+
+    for (int i = 2; i <= sqrt(N); i++) {//Eratosthenes 
+        if (prime[i] == true) {
+            for (int j = i * 2; j <= N; j += i) {// delete i*N
+                prime[j] = false;
+            }
+        }
+    }
+    for (int i = M; i <= N; i++) {
+        if (prime[i] == true)
+        {
+            if (min_value == -1)
+            {
+                min_value = i;
+            }
+        sum += i;
+        }
+}
+    if (min_value == -1)
+        cout << "-1";
+    else
+        cout << sum << '\n' << min_value;
+    return 0;
+}
