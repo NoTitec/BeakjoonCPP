9370ee4411182f1dbccaa7f6c7004049e38707b7
NoTitec <zkxkr0@gmail.com>
2023-09-07 21:41:32 +0900

    2439까지

---
+++ 2438/2438.cpp
@@ -0,0 +1,22 @@
+﻿// 2438.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 0; j < i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
+
+
---
+++ 8393/8393.cpp
@@ -0,0 +1,19 @@
+﻿// 8393.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+int result;//결과 
+int inputnum;//입력 수
+int main()
+{
+    cin >> inputnum;
+    for (int i = 1; i <= inputnum; i++) {
+        result = result + i;
+       
+    }
+    cout << result;
+}
+
+
